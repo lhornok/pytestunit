ddf7a713ef68288a7cd29753ef0e118f657d93f3
lhornok <lhornok@gmail.com>
2020-10-08 14:37:50 +0000

    modif

--- foxtests.py
+++ foxtests.py
@@ -3,6 +3,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 import page
 
@@ -17,7 +18,7 @@ class FoxCommand(unittest.TestCase):
         self.options.headless=True
         #self.driver = webdriver.Firefox(firefox_options=self.options)
         self.driver = webdriver.Firefox(options=self.options)
-        self.driver.get("http://192.168.34.30/prestashop/fr/")
+        self.driver.get("http://localhost:8002/prestashop/fr/")
 
     def tests(self):
         """
@@ -30,20 +31,19 @@ class FoxCommand(unittest.TestCase):
         """
 
         main_page = page.MainPage(self.driver)
+        WebDriverWait(self.driver,20).until(expected_conditions.title_contains('DFTG'))
         assert main_page.is_title_matches(), "Title DFTG not found ..."
         main_page.search_text_element = "renard"
         main_page.click_search()
-        time.sleep(2)
+        WebDriverWait(self.driver,20).until(expected_conditions.title_contains('Rechercher'))
         main_page.link_article()
-        time.sleep(2)
+        WebDriverWait(self.driver,20).until(expected_conditions.title_contains('Coussin'))
         main_page.put_art_qty()
-        time.sleep(2)
         main_page.add_to_cart()
-        time.sleep(2)
         main_page.back()
         time.sleep(2)
         main_page.display_cart()
-        time.sleep(2)
+        WebDriverWait(self.driver,20).until(expected_conditions.title_contains('Panier'))
         assert main_page.check_article_cart(), "Article name not match ..."
         assert main_page.check_article_qty_cart(), "Article quantity not match ..."
 
